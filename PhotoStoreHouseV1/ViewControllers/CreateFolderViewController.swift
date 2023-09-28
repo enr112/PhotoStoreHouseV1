33de2abf6634ab574cd1eef0576d35ad482704ea
Enrique <someemail@gmail.com>
2023-09-27 21:40:19 -0400

    added a CreateFolderViewController  file and added a ViewController in Storyboard

---
+++ PhotoStoreHouseV1/ViewControllers/CreateFolderViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateFolderViewController.swift
+//  PhotoStoreHouseV1
+//
+//  Created by Loaner on 9/27/23.
+//
+
+import UIKit
+
+class CreateFolderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
