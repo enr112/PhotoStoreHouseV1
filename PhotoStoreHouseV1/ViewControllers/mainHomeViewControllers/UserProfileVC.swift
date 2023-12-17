08c9e334b5fabf044fc8c278721ac7210cf123c4
Enrique <someemail@gmail.com>
2023-12-17 12:41:28 -0500

    New Function created in UserProfileVC to set up user information when My Account button is tapped

--- PhotoStoreHouseV1/ViewControllers/mainHomeViewControllers/UserProfileVC.swift
+++ PhotoStoreHouseV1/ViewControllers/mainHomeViewControllers/UserProfileVC.swift
@@ -8,11 +8,35 @@
 import UIKit
 
 class UserProfileVC: UIViewController {
-
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    @IBOutlet weak var firstNameLabel: UILabel!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var lastNameLabel: UILabel!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var accessLevelLabel: UILabel!
+    @IBOutlet weak var accessLevelTextField: UITextField!
+    
+    @IBOutlet weak var changePasswordButton: UIButton!
+    // private var userDoc:UserDocument?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.title = "My Profile"
+        
+        errorLabel.isHidden = true
         // Do any additional setup after loading the view.
+        setupUserInfo()
+        
     }
     
 
@@ -26,4 +50,28 @@ class UserProfileVC: UIViewController {
     }
     */
 
+    func setupUserInfo(){
+        let userObj = ManageUsers()
+        
+        userObj.getUserDocument { userDoc, error in
+            
+            if let error = error {
+                print("Error retrieving current user: \(error.localizedDescription)")
+                self.errorLabel.text = "Error retrieving current user information"
+                self.errorLabel.isHidden = false
+            } else if let userDoc = userDoc {
+                print("User fetched successfully: \(userDoc)")
+                self.firstNameTextField.text = userDoc.firstName
+                self.lastNameTextField.text = userDoc.lastName
+                self.emailTextField.text = userDoc.email
+                self.accessLevelTextField.text = userDoc.accessLevel.codingKey.stringValue
+                
+            } else {
+                print("User document not found or Data is incomplete or of wrong type")
+                self.errorLabel.text = "User document not found or Data is incomplete or of wrong type"
+                self.errorLabel.isHidden = false
+            }
+        }
+    }
+    
 }
